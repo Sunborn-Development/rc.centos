b570cbddc1af8d53a2c1478933d1ca3b1eea8021
Jin Kimura <kimura.jin@sunborn.co.jp>
2016-02-21 22:01:36 +0900

    fix iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -11,16 +11,14 @@
 /sbin/iptables -A OUTPUT -o lo -j ACCEPT
 
 /sbin/iptables -A INPUT -s 10.0.0.0/8 -j DROP
-/sbin/iptables -A INPUT -s 172.16.0.0/12 -j ACCEPT
-/sbin/iptables -A INPUT -s 192.168.0.0/16 -j ACCEPT
+/sbin/iptables -A INPUT -s 172.16.0.0/12 -j DROP
+/sbin/iptables -A INPUT -s 192.168.0.0/16 -j DROP
 
 /sbin/iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
-/sbin/iptables -A INPUT -s 203.143.96.11,157.14.210.204,203.143.99.206 -p tcp --dport 22345 -j ACCEPT
+/sbin/iptables -A INPUT -s 203.143.96.11,157.14.210.204 -p tcp --dport 22345 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-/sbin/iptables -A INPUT -s 203.143.96.11,157.14.210.204,203.143.99.206 -p tcp --dport 40000:40010 -j ACCEPT
-/sbin/iptables -A INPUT -s 203.143.96.11,157.14.210.204,203.143.99.206 -p tcp --dport 21 -j ACCEPT
 
 /sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
