7cd040e629c9dd1f0c3f8b9c74277f1c675d509c
Jin Kimura <kimura.jin@sunborn.co.jp>
2016-02-21 20:25:54 +0900

    add scripts to iptables.sh chkconfig.sh

---
+++ scripts/chkconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/sbin/chkconfig auditd off
+/sbin/chkconfig haldaemon off
+/sbin/chkconfig lvm2-monitor off
+/sbin/chkconfig mdmonitor off
+/sbin/chkconfig messagebus off
+/sbin/chkconfig netfs off
+/sbin/chkconfig restorecond off
